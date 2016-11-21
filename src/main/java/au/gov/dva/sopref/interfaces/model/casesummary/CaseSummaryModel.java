8fa7d3510c12b1178f268f8e5c874e84d6e344ad
Nick Miller <nick.miller@govlawtech.com.au>
2016-11-21 11:10:43 +1100

    setup for TB

---
+++ src/main/java/au/gov/dva/sopref/interfaces/model/casesummary/CaseSummaryModel.java
@@ -0,0 +1,5 @@
+package au.gov.dva.sopref.interfaces.model.casesummary;
+
+public interface CaseSummaryModel {
+    // TB todo: add members here for data necessary to generate case summary document
+}
--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -2,6 +2,7 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+
 public class ExampleTest {
     @Test
     public void evaluatesExpression() {
