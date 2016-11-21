ec140390677161dbc1d4f769ff7e7f2d93965290
Nick Miller <nick.miller@govlawtech.com.au>
2016-11-21 16:01:37 +1100

    # This is a combination of 2 commits. # The first commit's message is:
    merged
    
    # This is the 2nd commit message:
    
    limit travis builds to main branches

---
+++ src/main/java/au/gov/dva/sopref/casesummary/CaseSummary.java
@@ -0,0 +1,11 @@
+package au.gov.dva.sopref.casesummary;
+
+import au.gov.dva.sopref.interfaces.model.casesummary.CaseSummaryModel;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CaseSummary {
+   public static CompletableFuture<byte[]> createCaseSummary(CaseSummaryModel caseSummaryModel) {
+       return null;
+   }
+}
---
+++ src/test/java/CaseSummaryModelMock.java
@@ -0,0 +1,6 @@
+import au.gov.dva.sopref.interfaces.model.casesummary.CaseSummaryModel;
+
+public class CaseSummaryModelMock implements CaseSummaryModel{
+
+    // TB todo: make a test implementation here
+}
