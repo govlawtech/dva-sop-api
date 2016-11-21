9c23e60535426ff8cdd4c13e1e7c05e6b3fdc932
Tobias Brown <brown.tobias@gmail.com>
2016-11-22 10:10:30 +1100

    Added Service interface

---
+++ src/main/java/au/gov/dva/sopref/interfaces/model/Service.java
@@ -0,0 +1,12 @@
+package au.gov.dva.sopref.interfaces.model;
+
+import com.google.common.collect.ImmutableSet;
+
+import java.time.LocalDate;
+
+public interface Service {
+    String getName();
+    String getType();
+    LocalDate getStartDate();
+    LocalDate getEndDate();
+}
