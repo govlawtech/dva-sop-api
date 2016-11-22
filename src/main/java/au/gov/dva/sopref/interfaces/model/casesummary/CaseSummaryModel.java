38d60044b97604005f51e8d4e388c63577c3a723
Tobias Brown <brown.tobias@gmail.com>
2016-11-22 12:43:54 +1100

    Added CaseSummaryModel interface

--- src/main/java/au/gov/dva/sopref/interfaces/model/casesummary/CaseSummaryModel.java
+++ src/main/java/au/gov/dva/sopref/interfaces/model/casesummary/CaseSummaryModel.java
@@ -1,30 +1,46 @@
 package au.gov.dva.sopref.interfaces.model.casesummary;
 
-import au.gov.dva.sopref.interfaces.model.Factor;
-import au.gov.dva.sopref.interfaces.model.Operation;
-import au.gov.dva.sopref.interfaces.model.SoP;
-import com.google.common.collect.ImmutableSet;
+import au.gov.dva.sopref.interfaces.model.*;
 
-import java.time.LocalDate;
 
 public interface CaseSummaryModel {
-    // TB todo: add members here for data necessary to generate case summary document
-
     /*
-    * Need the following:
+    * INPUTS:
     * Condition
-    * Date of onset
-    * Service history (list of operations)
-    * SOP title (Statement of Principles concerning JOINT INSTABILITY No. 32 of 2010)
-    * SOP type (BOP)
-    * SOP link
-    * Factors connected to service
+    * - Condition name
+    * - Condition ICD code
+    * - Acute condition / Accumulated over time (wear and tear)
+    * - Date of onset
+    * - Date of aggravation (range)
+    *
+    * Service history
+    * - Date of enlistment
+    * - Date of separation
+    * - Original hire date
+    * - Services history (list)
+    * - - Service name (eg. RAN)
+    * - - Service type (eg. Regular/Permanent force)
+    * - - Start date
+    * - - End date
+    *
+    * Operations history (list)
+    * - Operation name
+    * - Start date
+    * - End date
+    * - "Specified Area" date (CADF - MRCA 6(1)(b))
+    *
+    * OUTPUTS:
+    * SOP
+    * - SOP title (Statement of Principles concerning JOINT INSTABILITY No. 32 of 2010)
+    * - SOP type (BOP)
+    * - SOP URL
+    * - Applicable SOP factors (list)
+    * - Factors connected to service (list)
+    * - Progress towards threshold (wear and tear factors that include a threshold level of work and aren't satisfied)
     *
     * */
 
-//    String getCondition();
-//    LocalDate getConditionOnsetDate();
-//    ImmutableSet<Operation> getServiceHistory();
-//    SoP getSop();
-//    ImmutableSet<Factor> getFactorsConnectedToService();
+    Condition getCondition();
+    ServiceHistory getServiceHistory();
+    SoP getSop();
 }
