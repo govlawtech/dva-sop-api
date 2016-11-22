d9d7d46f2f249055f5f59def113ea74fc5829783
Tobias Brown <brown.tobias@gmail.com>
2016-11-22 14:14:49 +1100

    DVA-16 Updated constructor in CaseSummaryModelMock

--- src/test/java/CaseSummaryModelMock.java
+++ src/test/java/CaseSummaryModelMock.java
@@ -3,28 +3,18 @@ import au.gov.dva.sopref.interfaces.model.ServiceHistory;
 import au.gov.dva.sopref.interfaces.model.SoP;
 import au.gov.dva.sopref.interfaces.model.casesummary.CaseSummaryModel;
 
-import java.time.LocalDate;
-
 public class CaseSummaryModelMock implements CaseSummaryModel{
 
     // TB todo: make a test implementation here
 
-    private ConditionMock _condition = new ConditionMock();
-    private ServiceHistoryMock _serviceHistory;
+    private Condition _condition;
+    private ServiceHistory _serviceHistory;
     private SoP _sop;
 
-    public CaseSummaryModelMock() {
-        _condition.setName("lumbar spondylosis");
-        _condition.setICDCode("ICD-10-AM");
-        _condition.setType("acute");
-        _condition.setOnsetStartDate(LocalDate.of(2008, 2, 18));
-
-        _serviceHistory.setEnlistmentDate(LocalDate.of(2006, 1, 10));
-        _serviceHistory.setSeparationDate(LocalDate.of(2015, 11, 23));
-        _serviceHistory.setHireDate(LocalDate.of(2006, 6, 28));
-
-//        _serviceHistory.setServices();
-//        _serviceHistory.setOperations();
+    public CaseSummaryModelMock(Condition condition, ServiceHistory serviceHistory, SoP sop) {
+        _condition = condition;
+        _serviceHistory = serviceHistory;
+        _sop = sop;
     }
 
     public Condition getCondition() {
