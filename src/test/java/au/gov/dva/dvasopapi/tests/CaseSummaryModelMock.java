1a4f9a7197282c92b3fa42c8ff8cf05ec250e764
Tobias Brown <brown.tobias@gmail.com>
2016-11-23 16:00:42 +1100

    DVA-16 Added dummy implementation of CaseSummaryModel interface

--- src/test/java/au/gov/dva/dvasopapi/tests/CaseSummaryModelMock.java
+++ src/test/java/au/gov/dva/dvasopapi/tests/CaseSummaryModelMock.java
@@ -1,8 +1,42 @@
 package au.gov.dva.dvasopapi.tests;
 
+import au.gov.dva.sopref.interfaces.model.Condition;
+import au.gov.dva.sopref.interfaces.model.ServiceHistory;
+import au.gov.dva.sopref.interfaces.model.SoP;
 import au.gov.dva.sopref.interfaces.model.casesummary.CaseSummaryModel;
 
 public class CaseSummaryModelMock implements CaseSummaryModel{
 
     // TB todo: make a test implementation here
+
+    private Condition _condition;
+    private ServiceHistory _serviceHistory;
+    private SoP _sop;
+
+    public CaseSummaryModelMock() {
+    }
+
+    public Condition getCondition() {
+        return _condition;
+    }
+
+    public void setCondition(Condition condition) {
+        _condition = condition;
+    }
+
+    public ServiceHistory getServiceHistory() {
+        return _serviceHistory;
+    }
+
+    public void setServiceHistory(ServiceHistory serviceHistory) {
+        _serviceHistory = serviceHistory;
+    }
+
+    public SoP getSop() {
+        return _sop;
+    }
+
+    public void setSop(SoP sop) {
+        _sop = sop;
+    }
 }
