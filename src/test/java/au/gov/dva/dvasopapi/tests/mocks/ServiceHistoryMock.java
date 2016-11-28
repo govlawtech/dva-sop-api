2bd5242042ba6559c7a8c70936ff68ef6cdaf287
Tobias Brown <brown.tobias@gmail.com>
2016-11-28 16:08:01 +1100

    Updated mock service history

--- src/test/java/au/gov/dva/dvasopapi/tests/mocks/ServiceHistoryMock.java
+++ src/test/java/au/gov/dva/dvasopapi/tests/mocks/ServiceHistoryMock.java
@@ -1,59 +1,68 @@
+package au.gov.dva.dvasopapi.tests.mocks;
+
 import au.gov.dva.sopref.interfaces.model.Operation;
 import au.gov.dva.sopref.interfaces.model.Service;
 import au.gov.dva.sopref.interfaces.model.ServiceHistory;
 import com.google.common.collect.ImmutableSet;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public class ServiceHistoryMock implements ServiceHistory {
 
-    private LocalDate _enlistmentDate;
-    private LocalDate _separationDate;
-    private LocalDate _hireDate;
-    private ImmutableSet<Service> _services;
-    private ImmutableSet<Operation> _operations;
-
-    public ServiceHistoryMock() {
-
-    }
-
     public LocalDate getEnlistmentDate() {
-        return _enlistmentDate;
-    }
-
-    public void setEnlistmentDate(LocalDate enlistmentDate) {
-        _enlistmentDate = enlistmentDate;
+        return LocalDate.of(2004, 7, 1);
     }
 
     public LocalDate getSeparationDate() {
-        return _separationDate;
-    }
-
-    public void setSeparationDate(LocalDate separationDate) {
-        _separationDate = separationDate;
+        return LocalDate.of(2010, 6, 30);
     }
 
     public LocalDate getHireDate() {
-        return _hireDate;
-    }
-
-    public void setHireDate(LocalDate hireDate) {
-        _hireDate = hireDate;
+        return LocalDate.of(2004, 7, 20);
     }
 
     public ImmutableSet<Service> getServices() {
-        return _services;
-    }
+        return ImmutableSet.of(new Service() {
+            @Override
+            public String getName() {
+                return "Royal Australian Air Force";
+            }
 
-    public void setServices(ImmutableSet<Service> services) {
-        _services = services;
+            @Override
+            public String getType() {
+                return "Regular/Permanent Force";
+            }
+
+            @Override
+            public LocalDate getStartDate() {
+                return LocalDate.of(2004, 8, 1);
+            }
+
+            @Override
+            public LocalDate getEndDate() {
+                return LocalDate.of(2010, 6, 30);
+            }
+        });
     }
 
     public ImmutableSet<Operation> getOperations() {
-        return _operations;
+        return ImmutableSet.of(new Operation() {
+            @Override
+            public String getName() {
+                return "Operation WARDEN";
+            }
+
+            @Override
+            public LocalDate getStartDate() {
+                return LocalDate.of(2006, 3, 1);
+            }
+
+            @Override
+            public Optional<LocalDate> getEndDate() {
+                return Optional.of(LocalDate.of(2006, 12, 31));
+            }
+        });
     }
 
-    public void setOperations(ImmutableSet<Operation> operations) {
-        _operations = operations;
-    }
 }
