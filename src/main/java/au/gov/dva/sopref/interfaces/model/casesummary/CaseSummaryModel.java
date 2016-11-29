ae8e539fda49da56c39c9851edee42417389334a
Tobias Brown <brown.tobias@gmail.com>
2016-11-29 11:36:08 +1100

    Added threshold progress and factors connected to service

--- src/main/java/au/gov/dva/sopref/interfaces/model/casesummary/CaseSummaryModel.java
+++ src/main/java/au/gov/dva/sopref/interfaces/model/casesummary/CaseSummaryModel.java
@@ -1,46 +1,12 @@
 package au.gov.dva.sopref.interfaces.model.casesummary;
 
 import au.gov.dva.sopref.interfaces.model.*;
-
+import com.google.common.collect.ImmutableSet;
 
 public interface CaseSummaryModel {
-    /*
-    * INPUTS:
-    * Condition
-    * - Condition name
-    * - Condition ICD code
-    * - Acute condition / Accumulated over time (wear and tear)
-    * - Date of onset
-    * - Date of aggravation (range)
-    *
-    * Service history
-    * - Date of enlistment
-    * - Date of separation
-    * - Original hire date
-    * - Services history (list)
-    * - - Service name (eg. RAN)
-    * - - Service type (eg. Regular/Permanent force)
-    * - - Start date
-    * - - End date
-    *
-    * Operations history (list)
-    * - Operation name
-    * - Start date
-    * - End date
-    * - "Specified Area" date (CADF - MRCA 6(1)(b))
-    *
-    * OUTPUTS:
-    * SOP
-    * - SOP title (Statement of Principles concerning JOINT INSTABILITY No. 32 of 2010)
-    * - SOP type (BOP)
-    * - SOP URL
-    * - Applicable SOP factors (list)
-    * - Factors connected to service (list)
-    * - Progress towards threshold (wear and tear factors that include a threshold level of work and aren't satisfied)
-    *
-    * */
-
     Condition getCondition();
     ServiceHistory getServiceHistory();
     SoP getSop();
+    String getThresholdProgress();
+    ImmutableSet<Factor> getFactorsConnectedToService();
 }
--- src/test/java/au/gov/dva/dvasopapi/tests/mocks/CaseSummaryModelMock.java
+++ src/test/java/au/gov/dva/dvasopapi/tests/mocks/CaseSummaryModelMock.java
@@ -1,9 +1,8 @@
 package au.gov.dva.dvasopapi.tests.mocks;
 
-import au.gov.dva.sopref.interfaces.model.Condition;
-import au.gov.dva.sopref.interfaces.model.ServiceHistory;
-import au.gov.dva.sopref.interfaces.model.SoP;
+import au.gov.dva.sopref.interfaces.model.*;
 import au.gov.dva.sopref.interfaces.model.casesummary.CaseSummaryModel;
+import com.google.common.collect.ImmutableSet;
 
 public class CaseSummaryModelMock implements CaseSummaryModel{
 
@@ -17,5 +16,70 @@ public class CaseSummaryModelMock implements CaseSummaryModel{
 
     public SoP getSop() {
         return new MockLumbarSpondylosisSop();
+    }
+
+    public String getThresholdProgress() {
+        return "10kg/week * 10 weeks = 100kg";
+    }
+
+    public ImmutableSet<Factor> getFactorsConnectedToService() {
+        return ImmutableSet.of(new Factor() {
+            @Override
+            public String getParagraph() {
+                return "6(a)";
+            }
+
+            @Override
+            public String getText() {
+                return "having inflammatory joint disease in the lumbar spine " +
+                        "before the clinical onset of lumbar spondylosis";
+            }
+
+            @Override
+            public ImmutableSet<DefinedTerm> getDefinedTerms() {
+                return ImmutableSet.of(new DefinedTerm() {
+                    @Override
+                    public String getTerm() {
+                        return "inflammatory joint disease";
+                    }
+
+                    @Override
+                    public String getDefinition() {
+                        return "means rheumatoid arthritis, reactive arthritis, " +
+                                "psoriatic arthropathy, ankylosing spondylitis, or " +
+                                "arthritis associated with Crohn’s disease or " +
+                                "ulcerative colitis";
+                    }
+                });
+            }
+        }, new Factor() {
+            @Override
+            public String getParagraph() {
+                return "6(m)";
+            }
+
+            @Override
+            public String getText() {
+                return "extreme forward flexion of the lumbar spine for a cumulative " +
+                        "total of at least 1 500 hours before the clinical onset of " +
+                        "lumbar spondylosis";
+            }
+
+            @Override
+            public ImmutableSet<DefinedTerm> getDefinedTerms() {
+                return ImmutableSet.of(new DefinedTerm() {
+                    @Override
+                    public String getTerm() {
+                        return "extreme forward flexion of the lumbar spine";
+                    }
+
+                    @Override
+                    public String getDefinition() {
+                        return "means being in a posture involving greater than 90 " +
+                                "degrees of trunk flexion";
+                    }
+                });
+            }
+        });
     }
 }
