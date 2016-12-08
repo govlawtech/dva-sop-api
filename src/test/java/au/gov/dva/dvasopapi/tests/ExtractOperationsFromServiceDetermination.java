e6ce2356417ad5f82c23e4be4058effbf1d7b8a8
Tobias Brown <brown.tobias@gmail.com>
2016-12-08 13:01:15 +1100

    Added test to check all non-warlike operations exist

--- src/test/java/au/gov/dva/dvasopapi/tests/ExtractOperationsFromServiceDetermination.java
+++ src/test/java/au/gov/dva/dvasopapi/tests/ExtractOperationsFromServiceDetermination.java
@@ -134,4 +134,143 @@ public class ExtractOperationsFromServiceDetermination {
         ImmutableList<Operation> results = ServiceDeterminations.extractOperations(pdfBytes);
         Assert.assertTrue(results.size() == 26);
     }
+
+    @Test
+    public void checkAllNonWarlikeOperationsExist() throws IOException {
+        URL inputDocx = Resources.getResource("F2016L00995.docx");
+        byte[] pdfBytes = Resources.toByteArray(inputDocx);
+        ImmutableList<Operation> results = ServiceDeterminations.extractOperations(pdfBytes);
+
+        Operation opMazurka = new StoredOperation("Mazurka",
+                LocalDate.of(1993, 1, 28), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opOsier = new StoredOperation("Osier",
+                LocalDate.of(1997, 1, 24), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opJointGuardian = new StoredOperation("Joint Guardian",
+                LocalDate.of(1999, 6, 11), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opPomelo = new StoredOperation("Pomelo",
+                LocalDate.of(2001, 1, 15), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opPaladin = new StoredOperation("Paladin",
+                LocalDate.of(2003, 4, 21), Optional.of(LocalDate.of(2006, 7, 11)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opPaladinII = new StoredOperation("Paladin",
+                LocalDate.of(2006, 7, 12), Optional.of(LocalDate.of(2006, 8, 14)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opPaladinIII = new StoredOperation("Paladin",
+                LocalDate.of(2006, 8, 15), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAnode = new StoredOperation("Anode",
+                LocalDate.of(2003, 7, 24), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opCitadel = new StoredOperation("Citadel",
+                LocalDate.of(2003, 8, 18), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opSpire = new StoredOperation("Spire",
+                LocalDate.of(2004, 5, 20), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAzure = new StoredOperation("Azure",
+                LocalDate.of(2005, 4, 10), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAstute = new StoredOperation("Astute",
+                LocalDate.of(2006, 5, 12), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opRamp = new StoredOperation("Ramp",
+                LocalDate.of(2006, 7, 20), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opQuickstep = new StoredOperation("Quickstep",
+                LocalDate.of(2006, 10, 31), Optional.of(LocalDate.of(2006, 12, 22)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opQuickstepTonga = new StoredOperation("Quickstep Tonga",
+                LocalDate.of(2006, 11, 18), Optional.of(LocalDate.of(2006, 11, 30)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opHedgerow = new StoredOperation("Hedgerow",
+                LocalDate.of(2008, 7, 28), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opNNMEOAL = new StoredOperation("NNMEOAL",
+                LocalDate.of(2011, 3, 31), Optional.of(LocalDate.of(2011, 10, 31)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAslan = new StoredOperation("Aslan",
+                LocalDate.of(2011, 9, 23), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAccordion = new StoredOperation("Accordion",
+                LocalDate.of(2014, 7, 1), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opManitou = new StoredOperation("Manitou",
+                LocalDate.of(2014, 7, 1), Optional.of(LocalDate.of(2015, 5, 13)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opManitouII = new StoredOperation("Manitou",
+                LocalDate.of(2015, 5, 14), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opOkra = new StoredOperation("Okra",
+                LocalDate.of(2014, 7, 1), Optional.of(LocalDate.of(2014, 8, 8)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opOkraII = new StoredOperation("Okra",
+                LocalDate.of(2014, 8, 9), Optional.of(LocalDate.of(2015, 9, 8)),
+                ServiceType.NON_WARLIKE);
+
+        Operation opOkraIII = new StoredOperation("Okra",
+                LocalDate.of(2015, 9, 9), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opAugury = new StoredOperation("Augury",
+                LocalDate.of(2014, 7, 4), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+
+        Operation opHawick = new StoredOperation("Hawick",
+                LocalDate.of(2014, 7, 21), Optional.empty(),
+                ServiceType.NON_WARLIKE);
+        
+        Assert.assertTrue(results.contains(opMazurka));
+        Assert.assertTrue(results.contains(opOsier));
+        Assert.assertTrue(results.contains(opJointGuardian));
+        Assert.assertTrue(results.contains(opPomelo));
+        Assert.assertTrue(results.contains(opPaladin));
+        Assert.assertTrue(results.contains(opPaladinII));
+        Assert.assertTrue(results.contains(opPaladinIII));
+        Assert.assertTrue(results.contains(opAnode));
+        Assert.assertTrue(results.contains(opCitadel));
+        Assert.assertTrue(results.contains(opSpire));
+        Assert.assertTrue(results.contains(opAzure));
+        Assert.assertTrue(results.contains(opAstute));
+        Assert.assertTrue(results.contains(opRamp));
+        Assert.assertTrue(results.contains(opQuickstep));
+        Assert.assertTrue(results.contains(opQuickstepTonga));
+        Assert.assertTrue(results.contains(opHedgerow));
+        Assert.assertTrue(results.contains(opNNMEOAL));
+        Assert.assertTrue(results.contains(opAslan));
+        Assert.assertTrue(results.contains(opAccordion));
+        Assert.assertTrue(results.contains(opManitou));
+        Assert.assertTrue(results.contains(opManitouII));
+        Assert.assertTrue(results.contains(opOkra));
+        Assert.assertTrue(results.contains(opOkraII));
+        Assert.assertTrue(results.contains(opOkraIII));
+        Assert.assertTrue(results.contains(opAugury));
+        Assert.assertTrue(results.contains(opHawick));
+    }
+
 }
