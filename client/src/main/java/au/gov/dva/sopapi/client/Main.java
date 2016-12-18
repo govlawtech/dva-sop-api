7cf67b8ac75e42b81c1ef7320e406fbb4b776e6a
Tobias Brown <brown.tobias@gmail.com>
2016-12-18 12:14:04 +1100

    Async call to getOperations

--- client/src/main/java/au/gov/dva/sopapi/client/Main.java
+++ client/src/main/java/au/gov/dva/sopapi/client/Main.java
@@ -1,5 +1,6 @@
 package au.gov.dva.sopapi.client;
 
+import au.gov.dva.sopapi.dtos.QueryParamLabels;
 import au.gov.dva.sopapi.dtos.sopref.OperationsResponseDto;
 import org.apache.commons.cli.*;
 
@@ -18,37 +19,29 @@ public class Main {
         // - instantiate SoPApiClient with correct URL
         // - execute SoPApiClient methods and print results to console
 
-        Options options = new Options();
-
-        // Common options
-        Option serviceOption = new Option("service", true, "Name of the service to call.");
-        serviceOption.setRequired(true);
-
-        Option urlOption = new Option("url", true, "URL of the server that the API is running on.");
-        urlOption.setRequired(true);
-
-        // getOperations options
-        Option declaredAfterOption = new Option("declaredAfter", true, "Find operations declared after this ISO local date (yyyy-mm-dd).");
-        declaredAfterOption.setRequired(true);
-
-        options.addOption(serviceOption);
-        options.addOption(urlOption);
-        options.addOption(declaredAfterOption);
-
+        Options options = getOptions();
         CommandLineParser parser = new DefaultParser();
+
         try {
             CommandLine cmd = parser.parse(options, args);
 
-            if(cmd.hasOption("url")) {
-                URL serverUrl = new URL(cmd.getOptionValue("url"));
-                SoPApiClient client = new SoPApiClient(serverUrl);
+            URL serverUrl = new URL(cmd.getOptionValue("url"));
+            String service = cmd.getOptionValue("service");
 
-                LocalDate declaredAfterDate = LocalDate.parse(cmd.getOptionValue("declaredAfter"));
-                OperationsResponseDto operationsResponse =
-                        client.getOperations(declaredAfterDate).get();
+            SoPApiClient client = new SoPApiClient(serverUrl, service);
+            String result = "";
 
-                System.out.println(operationsResponse.toJsonString(operationsResponse));
+            switch (service) {
+                case "getOperations":
+                    LocalDate declaredAfterDate = LocalDate.parse(cmd.getOptionValue(QueryParamLabels.QUERY_DATE));
+                    OperationsResponseDto operationsResponse = client.getOperations(declaredAfterDate).get();
+                    result = OperationsResponseDto.toJsonString(operationsResponse);
+                    break;
+                default:
+                    break;
             }
+
+            System.out.println(result);
         } catch (ParseException e) {
             printHelp(options);
             System.out.println(e);
@@ -67,4 +60,29 @@ public class Main {
         HelpFormatter helpFormatter = new HelpFormatter();
         helpFormatter.printHelp("dva-sop-api", options);
     }
+
+    private static Options getOptions() {
+        Options options = new Options();
+
+        // Common options
+        Option serviceOption = new Option("service", true,
+                "Name of the service to call.");
+        serviceOption.setRequired(true);
+
+        Option urlOption = new Option("url", true,
+                "URL of the server that the API is running on.");
+        urlOption.setRequired(true);
+
+        // getOperations options
+        Option declaredAfterOption = new Option("declaredAfter", true,
+                "Find operations declared after this ISO local date (yyyy-mm-dd).");
+        declaredAfterOption.setRequired(true);
+
+        options.addOption(serviceOption);
+        options.addOption(urlOption);
+        options.addOption(declaredAfterOption);
+
+        return options;
+    }
+
 }
--- client/src/main/java/au/gov/dva/sopapi/client/SoPApiClient.java
+++ client/src/main/java/au/gov/dva/sopapi/client/SoPApiClient.java
@@ -1,10 +1,19 @@
 package au.gov.dva.sopapi.client;
 
+import au.gov.dva.sopapi.dtos.DvaSopApiDtoError;
 import au.gov.dva.sopapi.dtos.IncidentType;
+import au.gov.dva.sopapi.dtos.QueryParamLabels;
 import au.gov.dva.sopapi.dtos.StandardOfProof;
 import au.gov.dva.sopapi.dtos.sopref.OperationsResponseDto;
 import au.gov.dva.sopapi.dtos.sopref.SoPRefDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.Response;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.concurrent.CompletableFuture;
@@ -12,11 +21,14 @@ import java.util.concurrent.CompletableFuture;
 public class SoPApiClient {
 
     private final URL baseUrl;
+    private final String _service;
+    private final String _serviceUrl;
 
-    public SoPApiClient(URL baseUrl)
+    public SoPApiClient(URL baseUrl, String service)
     {
-
         this.baseUrl = baseUrl;
+        this._service = service;
+        this._serviceUrl = this.baseUrl + "/" + this._service;
     }
 
     public CompletableFuture<SoPRefDto> getFactors(String conditionName, String icdCodeVersion, String icdCodeValue, IncidentType incidentType, StandardOfProof standardOfProof)
@@ -25,20 +37,39 @@ public class SoPApiClient {
         // - make async call to getSopFactors endpoint (need to set headers, look at Application.java to see what is expected)
         // - deserialize the response to SoPRefDto using Jackson
 
-
-
         // this is just here to stop Findbugs complaining - can remove later
         System.out.print(baseUrl);
-
 
         return null;
     }
 
     public CompletableFuture<OperationsResponseDto> getOperations(LocalDate declaredAfter)
     {
-        return null;
+        AsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+        CompletableFuture<OperationsResponseDto> promise = asyncHttpClient
+                .prepareGet(this._serviceUrl)
+                .setHeader("Accept", "application/json")
+                .addQueryParam(QueryParamLabels.QUERY_DATE, declaredAfter.toString())
+                .execute()
+                .toCompletableFuture()
+                .thenApply(response -> response.getResponseBody())
+                .thenApply(json -> fromJsonString(json.toString()));
+
+        return promise;
     }
 
+    private static OperationsResponseDto fromJsonString(String json)
+    {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new Jdk8Module());
+
+        try {
+            OperationsResponseDto operationsResponseDto =
+                    objectMapper.readValue(json, OperationsResponseDto.class);
+            return operationsResponseDto;
+        } catch (IOException e) {
+            throw new DvaSopApiDtoError(e);
+        }
+    }
 
     //todo: add method for SoP support once Nick sets up the return type DTOS.
 
