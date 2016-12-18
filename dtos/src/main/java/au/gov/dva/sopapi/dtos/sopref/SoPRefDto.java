a717d707f22babefc87d94ff513d80d0c987f023
Tobias Brown <brown.tobias@gmail.com>
2016-12-18 19:07:22 +1100

    Added to/from json functions

--- dtos/src/main/java/au/gov/dva/sopapi/dtos/sopref/SoPRefDto.java
+++ dtos/src/main/java/au/gov/dva/sopapi/dtos/sopref/SoPRefDto.java
@@ -1,7 +1,12 @@
 package au.gov.dva.sopapi.dtos.sopref;
 
+import au.gov.dva.sopapi.dtos.DvaSopApiDtoError;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
+import java.io.IOException;
 import java.util.List;
 
 public class SoPRefDto {
@@ -12,5 +17,31 @@ public class SoPRefDto {
     public SoPRefDto(List<SoPDto> soPDtos)
     {
         _sops = soPDtos;
+    }
+
+    public static String toJsonString(SoPRefDto soPRefDto)
+    {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new Jdk8Module());
+        String jsonString = null;
+
+        try {
+            jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(soPRefDto);
+        } catch (JsonProcessingException e) {
+            throw new DvaSopApiDtoError(e);
+        }
+
+        return jsonString;
+    }
+
+    public static SoPRefDto fromJsonString(String json)
+    {
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new Jdk8Module());
+
+        try {
+            SoPRefDto soPRefDto = objectMapper.readValue(json, SoPRefDto.class);
+            return soPRefDto;
+        } catch (IOException e) {
+            throw new DvaSopApiDtoError(e);
+        }
     }
 }
