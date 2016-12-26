11f9ce3532797a5f06b357ebf719d1015e5f9da1
Nick Miller <nick.miller@govlawtech.com.au>
2016-12-26 16:12:24 +1100

    Comment out entire LS tests.

--- src/test/scala/LumbarSpondylosisTests.scala
+++ src/test/scala/LumbarSpondylosisTests.scala
@@ -14,19 +14,19 @@ class LumbarSpondylosisTests extends FunSuite{
 
 
 
-  test("Parse entire RH LS SoP") {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933", "sops_rh/F2014L00933.pdf")
-    System.out.print(TestUtils.prettyPrint(StoredSop.toJson(result)))
-
-   // assert(result != null)
-  }
-
-  test("Parse entire BoP LS SoP")
-  {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930", "sops_bop/F2014L00930.pdf")
-    System.out.println(TestUtils.prettyPrint(StoredSop.toJson(result)))
- //   assert(result != null)
-  }
+//  test("Parse entire RH LS SoP") {
+//    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933", "sops_rh/F2014L00933.pdf")
+//    System.out.print(TestUtils.prettyPrint(StoredSop.toJson(result)))
+//
+//   // assert(result != null)
+//  }
+//
+//  test("Parse entire BoP LS SoP")
+//  {
+//    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930", "sops_bop/F2014L00930.pdf")
+//    System.out.println(TestUtils.prettyPrint(StoredSop.toJson(result)))
+// //   assert(result != null)
+//  }
 
 
 }
