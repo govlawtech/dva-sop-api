95a761bda2f3cf6e79c3b118c620a8c8126b22fa
Nick Miller <nick.miller@govlawtech.com.au>
2016-12-26 15:52:40 +1100

    Add test that always passes to same file as tests that mysteriously fail on CI.

--- src/test/scala/LumbarSpondylosisTests.scala
+++ src/test/scala/LumbarSpondylosisTests.scala
@@ -2,20 +2,20 @@
 package au.gov.dva.sopapi.tests.parsers;
 
 import au.gov.dva.dvasopapi.tests.TestUtils
-import au.gov.dva.sopapi.interfaces.model.SoP
 import au.gov.dva.sopapi.sopref.data.sops.StoredSop
-import au.gov.dva.sopapi.sopref.parsing.implementations.LsSoPFactory
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
-import scala.io.Source
 
 
 @RunWith(classOf[JUnitRunner])
 class LumbarSpondylosisTests extends FunSuite{
 
 
+  test("Sanity test test"){
+    assert(true)
+  }
 
   test("Parse entire RH LS SoP") {
     val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933","sops/rh/F2014L00933.pdf")
