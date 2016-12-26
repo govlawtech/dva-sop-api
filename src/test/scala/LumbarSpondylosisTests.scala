d663f77a0839c41364a0dddc12a16c2c957ed5e4
Nick Miller <nick.miller@govlawtech.com.au>
2016-12-26 12:47:38 +1100

    try double forwardslash for scala resource paths

--- src/test/scala/LumbarSpondylosisTests.scala
+++ src/test/scala/LumbarSpondylosisTests.scala
@@ -15,14 +15,14 @@ class LumbarSpondylosisTests extends FunSuite{
 
 
   test("Parse entire RH LS SoP") {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933","sops/rh/F2014L00933.pdf")
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00933","sops//rh//F2014L00933.pdf")
     System.out.print(TestUtils.prettyPrint(StoredSop.toJson(result)))
     assert(result != null)
   }
 
   test("Parse entire BoP LS SoP")
   {
-    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930","sops/bop/F2014L00930.pdf")
+    val result = ParserTestUtils.executeWholeParsingPipeline("F2014L00930","sops//bop//F2014L00930.pdf")
     System.out.println(TestUtils.prettyPrint(StoredSop.toJson(result)))
     assert(result != null)
   }
