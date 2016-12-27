c7d16af172b4cb3418989a0f5f31dc7c82c852d6
Nick Miller <nick.miller@govlawtech.com.au>
2016-12-27 17:23:52 +1100

    canary

--- src/test/scala/LumbarSpondylosisTests.scala
+++ src/test/scala/LumbarSpondylosisTests.scala
@@ -22,7 +22,7 @@ class LumbarSpondylosisTests extends FunSuite {
     val rawText = Conversions.pdfToPlainText(bytes);
     val genericClenser = new GenericClenser();
     val clensedText = genericClenser.clense(rawText)
-   // val sopFactory = SoPFactoryLocator.findFactory("F2014L00933")
+    val sopFactory = SoPFactoryLocator.findFactory("F2014L00933")
    // val sop = sopFactory.create("F2014L00933", clensedText)
     assert(clensedText != null)
   }
