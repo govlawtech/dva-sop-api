8801b0a09048988d48c1ffb4fc237e11da8b2802
michael-ctr <freezingdeep@yahoo.com>
2017-06-16 17:14:09 +1000

    fix staging/master name problem

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
 elif [ "$TRAVIS_BRANCH" == "devtest" ]; then
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
   curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/api-docs/yaml/
-elif [ "$TRAVIS_BRANCH" == "staging" ]; then
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$STAGING_USER:$STAGING_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
   curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$STAGING_USER:$STAGING_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/api-docs/yaml/
 fi
