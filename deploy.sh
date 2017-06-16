cce898575434ea87671a3313f610fcc860148d9d
michael-ctr <freezingdeep@yahoo.com>
2017-06-16 16:01:15 +1000

    take 13

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "dev" ]; then
+  ls app/deploy/webapps
+  echo $DEV_USER
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot
 fi
