3948941ec68506ab20c65fc08ff15269337c5662
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2017-06-19 13:16:02 +1000

    fix ftp user names and passwords to be env specific

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
   curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/api-docs/yaml/
 elif [ "$TRAVIS_BRANCH" == "devtest" ]; then
-  curl --ftp-create-dirs -T "app/web.config" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
+  curl --ftp-create-dirs -T "app/web.config" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
   curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/api-docs/yaml/
 elif [ "$TRAVIS_BRANCH" == "master" ]; then
-  curl --ftp-create-dirs -T "app/web.config" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
+  curl --ftp-create-dirs -T "app/web.config" -u "$STAGING_USER:$STAGING_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$STAGING_USER:$STAGING_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
   curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$STAGING_USER:$STAGING_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/api-docs/yaml/
 fi
