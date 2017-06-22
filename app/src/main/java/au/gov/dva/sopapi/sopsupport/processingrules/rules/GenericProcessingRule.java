5041225cec383e554b888189a2c063bed3f6d872
michael-ctr <freezingdeep@yahoo.com>
2017-06-22 14:18:28 +1000

    Fix for issue #7

--- app/src/main/java/au/gov/dva/sopapi/sopsupport/processingrules/rules/GenericProcessingRule.java
+++ app/src/main/java/au/gov/dva/sopapi/sopsupport/processingrules/rules/GenericProcessingRule.java
@@ -11,7 +11,8 @@ import au.gov.dva.sopapi.sopsupport.processingrules.RuleConfigRepositoryUtils;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
-import java.time.OffsetDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -23,9 +24,11 @@ import static au.gov.dva.sopapi.sopsupport.processingrules.RuleConfigRepositoryU
 public class GenericProcessingRule implements ProcessingRule {
 
     protected RuleConfigurationRepository ruleConfigurationRepository;
+    protected final OffsetDateTime mrcaStartDate;
 
     public GenericProcessingRule(RuleConfigurationRepository ruleConfigurationRepository) {
         this.ruleConfigurationRepository = ruleConfigurationRepository;
+        this.mrcaStartDate = OffsetDateTime.parse("2004-07-01T00:00:00+00:00");
     }
 
     public Optional<SoP> getApplicableSop(Condition condition, ServiceHistory serviceHistory, Predicate<Deployment> isOperational, CaseTrace caseTrace) {
@@ -33,6 +36,11 @@ public class GenericProcessingRule implements ProcessingRule {
         Optional<Rank> relevantRank = ProcessingRuleFunctions.getRankProximateToDate(serviceHistory.getServices(), condition.getStartDate(), caseTrace);
         if (!relevantRank.isPresent()) {
             caseTrace.addReasoningFor(ReasoningFor.ABORT_PROCESSING, "Cannot determine the relevant rank, therefore cannot apply STP rules to determine the applicable SoP.");
+            return Optional.empty();
+        }
+
+        if (serviceHistory.getHireDate().isBefore(mrcaStartDate)) {
+            caseTrace.addReasoningFor(ReasoningFor.ABORT_PROCESSING, "Cannot currently apply STP rules for veterans hired on or before 30 June 2004");
             return Optional.empty();
         }
 
