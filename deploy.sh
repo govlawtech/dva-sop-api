24c747764b9a047426929474a18ed1ad2f09d543
michael-ctr <freezingdeep@yahoo.com>
2017-07-05 11:56:54 +1000

    disabled deployments to the non-existent dev environment since these seem to be stuffing up devtest

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "dev" ]; then
-  curl --ftp-create-dirs -T "app/web.config" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
-  curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
-  curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/api-docs/yaml/
+  echo "deployments to dev disabled"
+#  curl --ftp-create-dirs -T "app/web.config" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
+#  curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
+#  curl --ftp-create-dirs -T "app/deploy/webapps/api-docs/yaml/dva-sop-api.yaml" -u "$DEV_USER:$DEV_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/api-docs/yaml/
 elif [ "$TRAVIS_BRANCH" == "devtest" ]; then
   curl --ftp-create-dirs -T "app/web.config" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/
   curl --ftp-create-dirs -T "app/deploy/webapps/ROOT.war" -u "$DEVTEST_USER:$DEVTEST_PASSWORD" ftp://waws-prod-sy3-011.ftp.azurewebsites.windows.net/site/wwwroot/webapps/
