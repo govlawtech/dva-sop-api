1112ea6fd3459910e3fbf365fada99db0fb6bf15
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2018-02-23 16:38:58 +1100

    draft of sub part decorator

--- app/src/main/java/au/gov/dva/sopapi/sopsupport/processingrules/rules/FactorSubPartDecorator.java
+++ app/src/main/java/au/gov/dva/sopapi/sopsupport/processingrules/rules/FactorSubPartDecorator.java
@@ -1,6 +1,7 @@
 package au.gov.dva.sopapi.sopsupport.processingrules.rules;
 
 import au.gov.dva.sopapi.dtos.Recommendation;
+import au.gov.dva.sopapi.exceptions.ProcessingRuleRuntimeException;
 import au.gov.dva.sopapi.interfaces.*;
 import au.gov.dva.sopapi.interfaces.model.*;
 import au.gov.dva.sopapi.sopref.parsing.traits.ParaReferenceSplitter;
@@ -14,17 +15,13 @@ import java.util.stream.Collectors;
 
 public class FactorSubPartDecorator extends ProcessingRuleBase implements ProcessingRule {
 
-    private final ProcessingRule toDecorate;
-    private final ConditionConfiguration conditionConfiguration;
-    private final ParaReferenceSplitter paraReferenceSplitter;
 
-    public FactorSubPartDecorator(ProcessingRule toDecorate, ConditionConfiguration conditionConfiguration, ParaReferenceSplitter paraReferenceSplitter)
+    private final ProcessingRule toDecorate;
+
+    public FactorSubPartDecorator(ProcessingRule toDecorate, ConditionConfiguration conditionConfiguration)
     {
         super(conditionConfiguration);
-
         this.toDecorate = toDecorate;
-        this.conditionConfiguration = conditionConfiguration;
-        this.paraReferenceSplitter = paraReferenceSplitter;
     }
 
 
@@ -36,28 +33,61 @@ public class FactorSubPartDecorator extends ProcessingRuleBase implements Proces
     @Override
     public ImmutableList<FactorWithSatisfaction> getSatisfiedFactors(Condition condition, SoP applicableSop, ServiceHistory serviceHistory, CaseTrace caseTrace) {
 
+        ImmutableList<FactorWithSatisfaction> factors = toDecorate.getSatisfiedFactors(condition,applicableSop,serviceHistory,caseTrace);
+        Optional<ApplicableRuleConfiguration> applicableRuleConfiguration = super.getApplicableRuleConfiguration(serviceHistory,condition,caseTrace);
+        if (!applicableRuleConfiguration.isPresent()) throw new ProcessingRuleRuntimeException("Could not determine applicable rule configuration.");
+        Optional<? extends RuleConfigurationItem> applicableRuleConfigurationItem = applicableRuleConfiguration.get().getRuleConfigurationForStandardOfProof(applicableSop.getStandardOfProof());
+        if (!applicableRuleConfigurationItem.isPresent()) throw new ProcessingRuleRuntimeException("Could not determine applicable rule configuration item.");
 
-        ApplicableRuleConfiguration applicableRuleConfiguration = super.getApplicableRuleConfiguration(serviceHistory,condition,caseTrace).get();
-        Optional<? extends RuleConfigurationItem> applicableRuleConfigurationItem = applicableRuleConfiguration.getRuleConfigurationForStandardOfProof(applicableSop.getStandardOfProof());
+        ImmutableList<FactorWithSatisfaction> addornedWithSubPart = factors.stream()
+                .map(f -> addornWithApplicablePartIfAny(applicableRuleConfigurationItem.get(),f))
+                .collect(Collectors.collectingAndThen(Collectors.toList(),ImmutableList::copyOf));
 
-        ImmutableList<String> factorRefereneces =  applicableRuleConfigurationItem.get().getMainFactorReferences().asList();
+        return addornedWithSubPart;
 
+    }
 
-        // needs to add applicable part
-        // identify sub factor reference if any
-        List<String> factorReferencesWhichContainSubParas = factorRefereneces.stream()
-                .filter(s -> paraReferenceSplitter.hasSubParas(s))
-                .collect(Collectors.toList());
+    private FactorWithSatisfaction addornWithApplicablePartIfAny(RuleConfigurationItem ruleConfigurationItem, FactorWithSatisfaction toAddorn)
+    {
+        String mainFactorRef = toAddorn.getFactor().getParagraph();
 
-        // reduce factor reference to core, then run decorated
+        Optional<FactorReference> factorReferenceWithSubPart = ruleConfigurationItem.getFactorRefObjects().stream()
+        .filter(factorReference -> factorReference.getMainFactorReference().contentEquals(mainFactorRef))
+        .filter(factorReference -> factorReference.getFactorPartReference().isPresent())
+        .findFirst();
 
+        if (factorReferenceWithSubPart.isPresent())
+        {
+            String applicablePart = factorReferenceWithSubPart.get().getFactorPartReference().get();
+            if (!factorContainsSubPart(toAddorn.getFactor(),applicablePart))
+            {
+                throw new ProcessingRuleRuntimeException(String.format("The subpart reference %s is not present in factor %s for instrument %s, condition %s", applicablePart, mainFactorRef, ruleConfigurationItem.getInstrumentId(), ruleConfigurationItem.getConditionName()));
+            }
 
-        return null;
+            return new SatisfiedFactorWithApplicablePart(toAddorn,applicablePart);
+        }
 
-
+        else {
+            return toAddorn;
+        }
 
 
     }
+
+    private boolean factorContainsSubPart(Factor factor, String subPartReference)
+    {
+        if (!factor.getConditionVariant().isPresent()) return false;
+
+        List<ConditionVariantFactor> variantFactors = factor.getConditionVariant().get()
+                .getVariantFactors().stream()
+                .filter(s -> s.getSubParagraph().contentEquals(subPartReference))
+                .collect(Collectors.toList());
+
+        return !variantFactors.isEmpty();
+
+    }
+
+
 
     @Override
     public void attachConfiguredFactorsToCaseTrace(Condition condition, ServiceHistory serviceHistory, CaseTrace caseTrace) {
