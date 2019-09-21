c00d895b3e702d9727eba7fa53a2680e246efcb4
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2019-09-22 07:52:51 +1000

    wip

--- app/src/main/scala/au/gov/dva/sopapi/sopref/dependencies/Dependencies.scala
+++ app/src/main/scala/au/gov/dva/sopapi/sopref/dependencies/Dependencies.scala
@@ -15,9 +15,23 @@ import scalax.collection.Graph
 import scalax.collection.GraphTraversal.{Predecessors, Successors}
 import scalax.collection.edge.LDiEdge
 
-abstract class FactorRef
-case class FactorConditionReference(dependentSop: SoP, dependentSopFactor: Factor, targetCondition: String) extends FactorRef
-case class FactorConditionReferenceWithTime(dependentSop: SoP, dependentSopFactor: Factor, targetCondition: String, days: Int) extends FactorRef
+case class FactorRef(dependentSop: SoP, linkingFactor: Factor, targetSoPPair : SoPPair) {
+    private def isOnset = dependentSop.getOnsetFactors.asScala.exists(f => f.getParagraph == linkingFactor.getParagraph)
+    private def getStandardOfProof = dependentSop.getStandardOfProof.toAbbreviatedString
+    private def getVariant = linkingFactor.getConditionVariant
+    def getLabel : String = {
+      val onsetLabel = if (isOnset) "onset" else "clinical worsening";
+      val para = linkingFactor.getParagraph
+      val variantLabel = if (getVariant.isPresent) s",${getVariant.get().getName}" else ""
+      s"($para: $onsetLabel,$getStandardOfProof$variantLabel)"
+    }
+
+  override def toString: String = getLabel
+}
+
+
+
+
 
 object Dependencies {
 
@@ -35,18 +49,14 @@ object Dependencies {
         case LDiEdge(source,target,label) => Some(
           (dotRoot,DotEdgeStmt(
             NodeId(source.toString()),
-            NodeId(target.toString())
+            NodeId(target.toString()),
+            Seq(DotAttr(Id("label"),Id(label.toString)))
           )))
       }
     }
-
     val g = buildGraph(sopPairs.asScala.toList)
-
     val testCondition = sopPairs.asScala.filter(sp => conditionWhitelist.asScala.contains(sp.getConditionName)).head
-
     val subGraph = liftSubGraphForCondition(g,testCondition)
-
-
     subGraph.toDot(dotRoot, edgeTransformer)
   }
 
@@ -59,53 +69,46 @@ object Dependencies {
     children.toList.mkString(Properties.lineSeparator)
   }
 
-  def getSopsMentioningPhraseInFactors(phrase : String, sopPairs: ImmutableSet[SoPPair]) : String = {
-    val referencingPairs = findSoPPairsReferencingPhraseInFactors(phrase,sopPairs.asScala.toList)
-    referencingPairs.map(p => p.getConditionName).mkString(Properties.lineSeparator)
-  }
 
   private def buildGraph(SoPPairs: List[SoPPair]): Graph[SoPPair, LDiEdge] = {
-
     val edges = buildEdges(SoPPairs)
-
-
     val g: Graph[SoPPair, LDiEdge] = Graph.from(SoPPairs,edges)
     g
   }
 
   private def buildEdges(SoPPairs: List[SoPPair]) : Seq[LDiEdge[SoPPair]] = {
-
     def createEdgesForSoPPairRecursive(remainingSoPPairs : List[SoPPair]): Seq[LDiEdge[SoPPair]] = {
       if (remainingSoPPairs.isEmpty) List()
       else createEdgesForSoPPair(remainingSoPPairs.head, SoPPairs.filter(s => s.getConditionName != remainingSoPPairs.head.getConditionName)) ++ createEdgesForSoPPairRecursive(remainingSoPPairs.tail)
     }
-
     createEdgesForSoPPairRecursive(SoPPairs)
   }
 
-  private def findSoPPairsReferencingPhraseInFactors(phrase: String, sopPairs: List[SoPPair]): List[SoPPair] = {
-
+  private def findFactorReferences(dependentSoPPair : SoPPair, otherSoPPairs : List[SoPPair]) : Map[SoPPair, List[FactorRef]] = {
     def definedTermsContainsPhrase(conditionName: String,  factor: Factor) = {
-            val definitions = factor.getDefinedTerms.asScala
-            definitions.exists(d => d.getDefinition.contains(phrase))
+      val definitions = factor.getDefinedTerms.asScala
+      definitions.exists(d => d.getDefinition.contains(conditionName))
     }
 
-    def findFactorsContainingConditionName(conditionName: String, SoPPair: SoPPair): List[Factor] = {
-      val allFactors: List[Factor] = (SoPPair.getRhSop.getOnsetFactors.asScala
-        ++ SoPPair.getRhSop.getAggravationFactors.asScala
-        ++ SoPPair.getBopSop.getOnsetFactors.asScala
-        ++ SoPPair.getBopSop.getAggravationFactors.asScala) toList
-
-      allFactors.filter(f => f.getText.contains(conditionName) || definedTermsContainsPhrase(conditionName, f))
+    def gatherFactorRefs(dependentSop: SoP, targetSopPair: SoPPair) : List[FactorRef] = {
+      def getFactorsReferencesingCondition(factors: List[Factor] ) = factors.filter(f => f.getText.contains(target  SopPair.getConditionName) || definedTermsContainsPhrase(targetSopPair.getConditionName,f))
+      def toFactorRef(f : Factor) = FactorRef(dependentSop,f,targetSopPair)
+      val onsetFactorsReferencingCondition = getFactorsReferencesingCondition(dependentSop.getOnsetFactors.asScala.toList)
+      val aggFactorsReferencingCondition = getFactorsReferencesingCondition(dependentSop.getAggravationFactors.asScala.toList)
+      (onsetFactorsReferencingCondition ++ aggFactorsReferencingCondition).map(toFactorRef(_))
     }
 
-    sopPairs.filter(SoPPair => findFactorsContainingConditionName(phrase, SoPPair).nonEmpty)
+    otherSoPPairs.flatMap(
+
+    )
   }
 
+
   private def createEdgesForSoPPair(sopPair : SoPPair, otherSoPPairs : List[SoPPair]): Seq[LDiEdge[SoPPair]] = {
-    val sources = findSoPPairsReferencingPhraseInFactors(sopPair.getConditionName,otherSoPPairs)
-    val target = sopPair
-    sources map (s => LDiEdge(s,target)(""))
+    val factorReferencesFromDependentSop:  List[FactorRef] = findFactorReferences(sopPair,otherSoPPairs)
+    val groupedByTarget: Map[SoPPair, List[FactorRef]] = factorReferencesFromDependentSop.groupBy(f => f.targetSoPPair)
+    groupedByTarget.map(fr => LDiEdge())
+    factorReferencesFromDependentSop.map(fr => LDiEdge(sopPair,fr.)(fr._2))
   }
 
   private def liftSubGraphForCondition(graph : Graph[SoPPair, LDiEdge], condition : SoPPair): Graph[SoPPair,LDiEdge] = {
@@ -113,13 +116,5 @@ object Dependencies {
     val subGraph = root.innerNodeTraverser.withDirection(Predecessors).withMaxDepth(1).toGraph
     subGraph
   }
-
-  // need function to link a factor to a condition, including in definitions
-  def getConditionDependenciesOfFactor(sourceSop: SoP, sourceFactor: Factor, conditionNames : Set[String]) : Set[FactorRef]  = {
-    // links can be in factor text or defined terms
-    // link can require onset by a certain date
-    ???
-  }
-
 }
 
