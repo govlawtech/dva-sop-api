4e3b843098af376175ace9e6b01949ef50bcae14
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2019-11-17 14:56:59 +1100

    amend agg/onset splitter to accomodate typo

--- app/src/main/scala/au/gov/dva/sopapi/sopref/parsing/implementations/parsers/PostAugust2015Parser.scala
+++ app/src/main/scala/au/gov/dva/sopapi/sopref/parsing/implementations/parsers/PostAugust2015Parser.scala
@@ -64,7 +64,7 @@ object PostAugust2015Parser extends SoPParser with PreAugust2015SoPParser {
   }
 
   override def parseStartAndEndAggravationParas(aggravationSection: String): (String, String) = {
-    val paraIntervalRegex = """subsections [0-9]+(\([0-9]+\)) (to|and) [0-9]+(\([0-9]+\))""".r
+    val paraIntervalRegex = """subsections? [0-9]+(\([0-9]+\)) (to|and) [0-9]+(\([0-9]+\))""".r
     val intervalMatch = paraIntervalRegex.findFirstMatchIn(aggravationSection)
     if (intervalMatch.isDefined) return (intervalMatch.get.group(1), intervalMatch.get.group(3))
 
