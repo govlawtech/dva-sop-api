4a0508f2d25f4155ff66ecc6d55131f37bbe7c7e
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2020-07-21 13:49:59 +1000

    modernise deployment script for new Azure powershell

--- tools/deployment scripts/clone staging storage to prod.ps1
+++ tools/deployment scripts/clone staging storage to prod.ps1
@@ -1,43 +1,26 @@
-﻿Param(
-    [Parameter(Mandatory = $true)]
-    [string]$SourceStorageAccount,
+﻿$SourceStorageContext = (Get-AzStorageAccount -ResourceGroupName govlawtechmainresourcegroup -Name dvasopapistoragedevtest).Context
+$DestStorageContext =  (Get-AzStorageAccount -ResourceGroupName govlawtechmainresourcegroup -Name dvasopapistorage).Context
 
-    [Parameter(Mandatory = $true)]
-    [string]$SourceStorageKey,
-
-    [Parameter(Mandatory = $true)]
-    [string]$DestStorageAccount,
-
-    [Parameter(Mandatory = $true)]
-    [string]$DestStorageKey
-)
-
-
-$SourceStorageContext = New-AzureStorageContext -StorageAccountName $SourceStorageAccount -StorageAccountKey $SourceStorageKey
-$DestStorageContext = New-AzureStorageContext -StorageAccountName $DestStorageAccount -StorageAccountKey $DestStorageKey
-
-$DestinationContainersToDelete = Get-AzureStorageContainer -Context $DestStorageContext 
+$DestinationContainersToDelete = Get-AzStorageContainer -Context $DestStorageContext 
 
 foreach ($SingleDestinationContainerToDelete in $DestinationContainersToDelete)
 {
-    Remove-AzureStorageContainer -Name $SingleDestinationContainerToDelete.Name -Context $SingleDestinationContainerToDelete.Context -Force
+    Remove-AzStorageContainer -Name $SingleDestinationContainerToDelete.Name -Context $SingleDestinationContainerToDelete.Context -Force
     Write-Output "Removed destination storage container: $SingleDestinationContainerToDelete."
 }
 
 Start-Sleep -Seconds 30
 
-$SourceContainers = Get-AzureStorageContainer -Context $SourceStorageContext | Where-Object {!$_.Name.StartsWith("$")}
+$SourceContainers = Get-AzStorageContainer -Context $SourceStorageContext | Where-Object {!$_.Name.StartsWith("$")}
 
 foreach($SingleSourceContainer in $SourceContainers)
 {
     $SingleSourceContainerName = $SingleSourceContainer.Name
-    if (!((Get-AzureStorageContainer -Context $DestStorageContext) | Where-Object { $_.Name -eq $SingleSourceContainerName }))
-    {   
-        Write-Output "Creating new target container '$SingleSourceContainerName'."
-        New-AzureStorageContainer -Name $SingleSourceContainerName -Permission Off -Context $DestStorageContext -ErrorAction Stop 
-    }
+    Write-Output "Creating new target container '$SingleSourceContainerName'."
+    New-AzStorageContainer -Name $SingleSourceContainerName -Permission Off -Context $DestStorageContext -ErrorAction Stop 
 
-    $Blobs = Get-AzureStorageBlob -Context $SourceStorageContext -Container $SingleSourceContainerName
+    
+    $Blobs = Get-AzStorageBlob -Context $SourceStorageContext -Container $SingleSourceContainerName
     $BlobCpyAry = @() #Create array of objects
 
     #Do the copy of everything
@@ -45,7 +28,7 @@ foreach($SingleSourceContainer in $SourceContainers)
     {
        $BlobName = $Blob.Name
        Write-Output "Copying $BlobName from $SingleSourceContainerName"
-       $BlobCopy = Start-CopyAzureStorageBlob -Context $SourceStorageContext -SrcContainer $SingleSourceContainerName -SrcBlob $BlobName -DestContext $DestStorageContext -DestContainer $SingleSourceContainerName -DestBlob $BlobName -Force
+       $BlobCopy = Start-AzStorageBlobCopy -Context $SourceStorageContext -SrcContainer $SingleSourceContainerName -SrcBlob $BlobName -DestContext $DestStorageContext -DestContainer $SingleSourceContainerName -DestBlob $BlobName -Force
        $BlobCpyAry += $BlobCopy
     }
 
@@ -53,7 +36,7 @@ foreach($SingleSourceContainer in $SourceContainers)
     foreach ($BlobCopy in $BlobCpyAry)
     {
       
-       $CopyState = $BlobCopy | Get-AzureStorageBlobCopyState
+       $CopyState = $BlobCopy | Get-AzStorageBlobCopyState
        $Message = $CopyState.Source.AbsolutePath + " " + $CopyState.Status + " {0:N2}%" -f (($CopyState.BytesCopied/$CopyState.TotalBytes)*100) 
        Write-Output $Message
     }
